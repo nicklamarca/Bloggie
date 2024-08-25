aa07e92d2d38e768554f1cb3b423b74a9134f929
Nick Lamarca <nicklamarca64@gmail.com>
2024-08-24 21:07:21 -0500

    Create DbContext And Run EF Migrations

---
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<BlogPost> BlogPosts { get; set; }
+        DbSet<Tag> Tags { get; set; }
+    }
+}
--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -1,3 +1,6 @@
+using Bloggie.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Bloggie.Web
 {
     public class Program
@@ -8,6 +11,11 @@ namespace Bloggie.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<BloggieDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbContext"));
+            });
 
             var app = builder.Build();
 
